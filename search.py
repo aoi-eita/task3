69edc6162bfde9ffd6286c1ef8e2030ed1a0d32d
aoi-eita <aoi.eita66@gmail.com>
2021-07-19 15:29:06 +0900

    課題３

---
+++ search.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import eel
+
+### デスクトップアプリ作成課題
+def kimetsu_search(word,csv_file):
+    # 検索対象取得
+    df=pd.read_csv("./"+csv_file)
+    source=list(df["name"])
+
+    # 検索
+    if word in source:
+        print(f"{word}はいます！！")
+        eel.view_log_js(f"{word}はいます！！")
+    else:
+        print(f"{word}はいません")
+        eel.view_log_js(f"{word}はいません")
+        # 追加
+        #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
+        #if add_flg=="1":
+        source.append(word)
+    
+    # CSV書き込み
+    df=pd.DataFrame(source,columns=["name"])
+    df.to_csv("./source.csv",encoding="utf_8-sig")
+    print(source)
+
+    return "最高"
